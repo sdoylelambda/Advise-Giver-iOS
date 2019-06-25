bc87437218b83af14bd633d38dd6f3b9f328069c
Sean Doyle <sdoylelambda@gmail.com>
2019-06-25 17:19:36 -0400

    Skeleton files added.

---
+++ AdviceGiver/AdviceGiver/ViewControllers/MyQDetailViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class MyQDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ AdviceGiver/AdviceGiver/ViewControllers/QViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class QViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+}
